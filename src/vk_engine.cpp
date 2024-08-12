6acc1a190beebfeff195ccee64d0f014ab1ef0c8
massanaRoger <94066091+massanaRoger@users.noreply.github.com>
2024-08-12 10:47:09 +0200

    Init swapchain

--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -51,7 +51,13 @@ void VulkanEngine::init()
 void VulkanEngine::cleanup()
 {
     if (_isInitialized) {
+        destroy_swapchain();
 
+        vkDestroySurfaceKHR(_instance, _surface, nullptr);
+        vkDestroyDevice(_device, nullptr);
+
+        vkb::destroy_debug_utils_messenger(_instance, _debug_messenger);
+        vkDestroyInstance(_instance, nullptr);
         SDL_DestroyWindow(_window);
     }
 
@@ -144,9 +150,10 @@ void VulkanEngine::init_vulkan()
     _device = vkbDevice.device;
     _chosenGPU = physicalDevice.physical_device;
 }
+
 void VulkanEngine::init_swapchain()
 {
-    //nothing yet
+    create_swapchain(_windowExtent.width, _windowExtent.height);
 }
 void VulkanEngine::init_commands()
 {
@@ -155,4 +162,31 @@ void VulkanEngine::init_commands()
 void VulkanEngine::init_sync_structures()
 {
     //nothing yet
+}
+
+void VulkanEngine::create_swapchain(uint32_t width, uint32_t height) {
+    vkb::SwapchainBuilder swapchainBuilder{ _chosenGPU, _device, _surface };
+    _swapchainImageFormat = VK_FORMAT_B8G8R8A8_UNORM;
+    vkb::Swapchain vkbSwapchain = swapchainBuilder
+        .set_desired_format(VkSurfaceFormatKHR{ .format = _swapchainImageFormat, .colorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR })
+        // Hard vsync
+        .set_desired_present_mode(VK_PRESENT_MODE_FIFO_KHR)
+        .set_desired_extent(width, height)
+        .add_image_usage_flags(VK_IMAGE_USAGE_TRANSFER_DST_BIT)
+        .build()
+        .value();
+
+    _swapchainExtent = vkbSwapchain.extent;
+    // Store swapchain and images
+    _swapchain = vkbSwapchain.swapchain;
+    _swapchainImages = vkbSwapchain.get_images().value();
+    _swapchainImageViews = vkbSwapchain.get_image_views().value();
+}
+
+void VulkanEngine::destroy_swapchain() {
+    vkDestroySwapchainKHR(_device, _swapchain, nullptr);
+
+    for (int i = 0; i < _swapchainImageViews.size(); i++) {
+        vkDestroyImageView(_device, _swapchainImageViews[i], nullptr);
+    }
 }
--- src/vk_engine.h
+++ src/vk_engine.h
@@ -19,6 +19,13 @@ public:
 	VkDevice _device;
 	VkSurfaceKHR _surface;
 
+	VkSwapchainKHR _swapchain;
+	VkFormat _swapchainImageFormat;
+
+	std::vector<VkImage> _swapchainImages;
+	std::vector<VkImageView> _swapchainImageViews;
+	VkExtent2D _swapchainExtent;
+
 	struct SDL_Window* _window{ nullptr };
 
 	static VulkanEngine& Get();
@@ -40,4 +47,7 @@ private:
 	void init_swapchain();
 	void init_commands();
 	void init_sync_structures();
+
+	void create_swapchain(uint32_t width, uint32_t height);
+	void destroy_swapchain();
 };
